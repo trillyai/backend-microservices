d3d250f04c3b6653cb7dde9177410f19409da396
Mehmet Ali MERGEN <mehmet.mergen@konzek.com>
2024-05-05 00:15:18 +0300

    impl handler

--- services/profile/handler/handler.go
+++ services/profile/handler/handler.go
@@ -1,9 +1,16 @@
 package handler
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/trillyai/backend-microservices/core/logger"
+	"github.com/trillyai/backend-microservices/core/utils"
 	"github.com/trillyai/backend-microservices/services/profile/contracts"
+	"github.com/trillyai/backend-microservices/services/profile/shared"
 )
 
 type handler struct {
@@ -18,17 +25,81 @@ func NewHandler(svc contracts.Service) contracts.Handler {
 	}
 }
 
+// //////////////////////////////////////////////////////////////////////////////////
 // GetProfile implements contracts.Handler.
-func (h handler) GetProfile(c *fiber.Ctx) error {
-	panic("unimplemented")
+// //////////////////////////////////////////////////////////////////////////////////
+func (handler handler) GetProfile(c *fiber.Ctx) error {
+	username := c.Params("username")
+	if strings.TrimSpace(username) == "" {
+		return errors.New("username required")
+	}
+
+	resp, err := handler.svc.GetProfileByUsername(c.Context(), username)
+	if err != nil {
+		handler.logger.Error(fmt.Sprintf("failed to process get profile: %v", err))
+		c.Status(fiber.StatusBadRequest).JSON(err.Error())
+		return err
+	}
+
+	c.Status(fiber.StatusOK).JSON(resp)
+	return nil
 }
 
+// //////////////////////////////////////////////////////////////////////////////////
 // GetProfiles implements contracts.Handler.
-func (h handler) GetProfiles(c *fiber.Ctx) error {
-	panic("unimplemented")
+// //////////////////////////////////////////////////////////////////////////////////
+func (handler handler) GetProfiles(c *fiber.Ctx) error {
+
+	offsetStr := c.Query("offset")
+	limitStr := c.Query("limit")
+
+	// Convert query parameters to uint32
+	offset, err := strconv.ParseUint(offsetStr, 10, 32)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString("Invalid offset value")
+	}
+
+	limit, err := strconv.ParseUint(limitStr, 10, 32)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString("Invalid limit value")
+	}
+
+	resp, err := handler.svc.GetProfiles(c.Context(), uint32(offset), uint32(limit))
+	if err != nil {
+		handler.logger.Error(fmt.Sprintf("failed to process get profiles: %v", err))
+		c.Status(fiber.StatusBadRequest).JSON(err.Error())
+		return err
+	}
+
+	c.Status(fiber.StatusOK).JSON(resp)
+	return nil
 }
 
+// //////////////////////////////////////////////////////////////////////////////////
 // UpdateProfile implements contracts.Handler.
-func (h handler) UpdateProfile(c *fiber.Ctx) error {
-	panic("unimplemented")
+// //////////////////////////////////////////////////////////////////////////////////
+func (handler handler) UpdateProfile(c *fiber.Ctx) error {
+	var req shared.UpdateProfileRequest
+
+	if err := c.BodyParser(&req); err != nil {
+		handler.logger.Error(fmt.Sprintf("failed to parse request body: %v", err))
+		c.Status(fiber.StatusBadRequest).JSON(err.Error())
+		return err
+	}
+
+	if err := utils.ValidateStruct(req); err != nil {
+		handler.logger.Error(err.Error())
+		c.Status(fiber.StatusBadRequest).JSON(err.Error())
+		return err
+	}
+
+	resp, err := handler.svc.UpdateProfile(c.Context(), req)
+	if err != nil {
+		handler.logger.Error(fmt.Sprintf("failed to process update-profile: %v", err))
+		c.Status(fiber.StatusBadRequest).JSON(err.Error())
+		return err
+	}
+
+	c.Status(fiber.StatusOK).JSON(resp)
+	return nil
 }
--- services/profile/repository/repository.go
+++ services/profile/repository/repository.go
@@ -2,7 +2,12 @@ package repository
 
 import (
 	"context"
+	"errors"
 
+	"github.com/google/uuid"
+	"github.com/trillyai/backend-microservices/core/auth"
+	"github.com/trillyai/backend-microservices/core/database/postgres"
+	"github.com/trillyai/backend-microservices/core/database/tables"
 	"github.com/trillyai/backend-microservices/core/logger"
 	"github.com/trillyai/backend-microservices/services/profile/contracts"
 	"github.com/trillyai/backend-microservices/services/profile/shared"
@@ -20,7 +25,17 @@ func NewRepository() contracts.Repository {
 
 // GetProfileByUsername implements contracts.Repository.
 func (r repository) GetProfileByUsername(ctx context.Context, username string) (shared.GetProfileResponse, error) {
-	panic("unimplemented")
+	resp, err := postgres.Read[shared.GetProfileResponse, tables.User](ctx, map[string]interface{}{"Username": username})
+	if err != nil {
+		r.logger.Error(err.Error())
+		return shared.GetProfileResponse{}, err
+	}
+
+	if resp.Id == uuid.Nil {
+		return shared.GetProfileResponse{}, errors.New("user not found")
+	}
+
+	return resp, nil
 }
 
 // GetProfiles implements contracts.Repository.
@@ -30,5 +45,9 @@ func (r repository) GetProfiles(ctx context.Context, offset uint32, limit uint32
 
 // UpdateProfile implements contracts.Repository.
 func (r repository) UpdateProfile(ctx context.Context, request shared.UpdateProfileRequest) (shared.UpdateProfileResponse, error) {
+	claims := ctx.Value("user").(*auth.Claims)
+	if claims.UserName == "" {
+		return shared.UpdateProfileResponse{}, errors.New("context not found")
+	}
 	panic("unimplemented")
 }
