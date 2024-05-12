ac517c7353a63efd2e1ca673a43e7b00741110e3
Mehmet Ali MERGEN <mehmet.mergen@konzek.com>
2024-05-12 14:49:24 +0300

    impl handler layer

--- services/interaction/handler/post.go
+++ services/interaction/handler/post.go
@@ -2,8 +2,11 @@ package handler
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"github.com/trillyai/backend-microservices/core/utils"
 	"github.com/trillyai/backend-microservices/services/interaction/shared"
 )
@@ -17,25 +20,20 @@ func (handler handler) CreatePost(c *fiber.Ctx) error {
 
 	if err := c.BodyParser(&req); err != nil {
 		handler.logger.Error(fmt.Sprintf("failed to parse request body: %v", err))
-		c.Status(fiber.StatusBadRequest).JSON(err.Error())
-		return err
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
 	}
 
 	if err := utils.ValidateStruct(req); err != nil {
 		handler.logger.Error(err.Error())
-		c.Status(fiber.StatusBadRequest).JSON(err.Error())
-		return err
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
 	}
 
 	resp, err := handler.svc.CreatePost(c.Context(), req)
 	if err != nil {
-		handler.logger.Error(fmt.Sprintf("failed to process create-post: %v", err))
-		c.Status(fiber.StatusBadRequest).JSON(err.Error())
-		return err
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
 	}
 
-	c.Status(fiber.StatusOK).JSON(resp)
-	return nil
+	return c.Status(fiber.StatusOK).JSON(resp)
 
 }
 
@@ -43,26 +41,113 @@ func (handler handler) CreatePost(c *fiber.Ctx) error {
 // UpdatePost implements contracts.Handler.
 // //////////////////////////////////////////////////////////////////////////////////
 func (handler handler) UpdatePost(c *fiber.Ctx) error {
-	panic("unimplemented")
+
+	var req shared.UpdatePostRequest
+
+	if err := c.BodyParser(&req); err != nil {
+		handler.logger.Error(fmt.Sprintf("failed to parse request body: %v", err))
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
+	}
+
+	if err := utils.ValidateStruct(req); err != nil {
+		handler.logger.Error(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
+	}
+
+	resp, err := handler.svc.UpdatePost(c.Context(), req)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
+	}
+
+	return c.Status(fiber.StatusOK).JSON(resp)
+
 }
 
 // //////////////////////////////////////////////////////////////////////////////////
 // DeletePost implements contracts.Handler.
 // //////////////////////////////////////////////////////////////////////////////////
 func (handler handler) DeletePost(c *fiber.Ctx) error {
-	panic("unimplemented")
+
+	var req shared.DeletePostRequest
+
+	if err := c.BodyParser(&req); err != nil {
+		handler.logger.Error(fmt.Sprintf("failed to parse request body: %v", err))
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
+	}
+
+	if err := utils.ValidateStruct(req); err != nil {
+		handler.logger.Error(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
+	}
+
+	resp, err := handler.svc.DeletePost(c.Context(), req)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
+	}
+
+	return c.Status(fiber.StatusOK).JSON(resp)
+
 }
 
 // //////////////////////////////////////////////////////////////////////////////////
 // GetPost implements contracts.Handler.
 // //////////////////////////////////////////////////////////////////////////////////
 func (handler handler) GetPost(c *fiber.Ctx) error {
-	panic("unimplemented")
+
+	postIdStr := c.Params("postId")
+
+	if strings.TrimSpace(postIdStr) == "" {
+		handler.logger.Error("postId param required")
+		return c.Status(fiber.StatusBadRequest).SendString("postId param required")
+	}
+
+	postId, err := uuid.Parse(postIdStr)
+	if err != nil {
+		handler.logger.Error(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
+	}
+
+	resp, err := handler.svc.GetPost(c.Context(), postId)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
+	}
+
+	return c.Status(fiber.StatusOK).JSON(resp)
+
 }
 
 // //////////////////////////////////////////////////////////////////////////////////
 // GetPosts implements contracts.Handler.
 // //////////////////////////////////////////////////////////////////////////////////
 func (handler handler) GetPosts(c *fiber.Ctx) error {
-	panic("unimplemented")
+
+	offsetStr := c.Query("offset")
+	limitStr := c.Query("limit")
+	userIdStr := c.Query("userId")
+
+	offset, err := strconv.ParseUint(offsetStr, 10, 32)
+	if err != nil {
+		handler.logger.Error(err.Error())
+		return c.Status(fiber.StatusBadRequest).SendString("Invalid offset value")
+	}
+
+	limit, err := strconv.ParseUint(limitStr, 10, 32)
+	if err != nil {
+		handler.logger.Error(err.Error())
+		return c.Status(fiber.StatusBadRequest).SendString("Invalid limit value")
+	}
+
+	userId, err := uuid.Parse(userIdStr)
+	if err != nil {
+		handler.logger.Error(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
+	}
+
+	resp, err := handler.svc.GetPosts(c.Context(), userId, uint32(offset), uint32(limit))
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
+	}
+
+	return c.Status(fiber.StatusOK).JSON(resp)
+
 }
